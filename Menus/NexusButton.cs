75eea29f02a3b970ab58edf089b4fb3a62a79f3b
Mike Watling <mike.watling@nexusmods.com>
2022-08-08 11:45:25 +0100

    Initial upload.

---
+++ Menus/NexusButton.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace NexusModsButton.Menus
+{
+    internal class NexusButton
+    {
+        private readonly ClickableTextureComponent nexusButton;
+        private bool wasUpdateButtonHovered;
+
+        internal bool ShowUpdateButton { get; set; }
+
+        private Point mousePosition = Point.Zero;
+
+        public NexusButton(IModHelper helper)
+        {
+            Texture2D buttonTexture = helper.Content.Load<Texture2D>("assets/nexusButton.png");
+
+            int position = 50;
+            // GenericModConfigMenu adds a button in the position we want to use. 
+            if (helper.ModRegistry.IsLoaded("spacechase0.GenericModConfigMenu")) {
+                position = 150;
+            }
+
+            // ModUpdateMenu adds a button in the position we use if GMCM is loaded, so move up again.
+            if (helper.ModRegistry.IsLoaded("cat.modupdatemenu") && helper.ModRegistry.IsLoaded("spacechase0.GenericModConfigMenu"))
+            {
+                position = 250;
+            }
+
+            this.nexusButton = new ClickableTextureComponent(
+                new Rectangle(36, Game1.viewport.Height - position - 48, 81, 75), buttonTexture, new Rectangle(0, 0, 27, 25),
+                3, false);
+
+            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+            helper.Events.Input.CursorMoved += this.OnCursorMoved;
+            helper.Events.Display.WindowResized += this.OnWindowResized;
+        }
+
+        /// <summary>Raised after tha game window is resized.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguements.</param>
+        private void OnWindowResized(object sender, WindowResizedEventArgs e)
+        {
+            this.nexusButton.bounds.Y = Game1.viewport.Height - 150 - 48;
+        }
+
+        /// <summary>Raised after the game state is updated (60 times per second).</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments</param>
+        private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            if (!this.ShowUpdateButton)
+                return;
+
+            if (this.mousePosition != Point.Zero)
+                this.nexusButton.tryHover(this.mousePosition.X, this.mousePosition.Y, 0.25f);
+        }
+
+        /// <summary>Raised after the player moves the in-game cursor.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnCursorMoved(object sender, CursorMovedEventArgs e)
+        {
+            if (!this.ShowUpdateButton)
+                return;
+
+            this.mousePosition = new Point((int)e.NewPosition.ScreenPixels.X, (int)e.NewPosition.ScreenPixels.Y);
+            bool isUpdateButtonHovered = this.nexusButton.containsPoint(this.mousePosition.X, this.mousePosition.Y);
+            if (isUpdateButtonHovered != this.wasUpdateButtonHovered)
+            {
+                this.nexusButton.sourceRect.X += this.wasUpdateButtonHovered ? -27 : 27;
+                if (!this.wasUpdateButtonHovered)
+                    Game1.playSound("Cowboy_Footstep");
+            }
+            this.wasUpdateButtonHovered = isUpdateButtonHovered;
+        }
+
+        internal bool PointContainsButton(Vector2 p)
+        {
+            return this.nexusButton.containsPoint((int)p.X, (int)p.Y);
+        }
+
+        public void Draw(SpriteBatch b)
+        {
+            if (!this.ShowUpdateButton)
+                return;
+            this.nexusButton.draw(Game1.spriteBatch);
+
+            if (Game1.activeClickableMenu is TitleMenu titleMenu)
+                titleMenu.drawMouse(b);
+        }
+
+    }
+}
---
+++ ModEntry.cs
@@ -0,0 +1,128 @@
+﻿using NexusModsButton.Menus;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using System.Diagnostics;
+
+namespace NexusModsButton
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+
+        /*********
+        ** Fields
+        *********/
+        /// <summary>The update button.</summary>
+        private NexusButton button;
+
+        /// <summary>The update menu.</summary>
+        // private UpdateMenu menu;
+
+        /// <summary>The mod configuration.</summary>
+        // private ModConfig config;
+
+        /// <summary>The current mod statuses.</summary>
+        // private IList<ModStatus> currentStatuses;
+
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {            
+            this.button = new NexusButton(helper);
+
+            // helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+            helper.Events.Display.Rendered += this.OnRendered;
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the game state is updated</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The events args.</param>
+        private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            this.button.ShowUpdateButton = this.ShouldDrawUpdateButton();
+        }
+
+        /// <summary>Raised after the game draws to the sprite patch in a draw tick, just before the final sprite batch is rendered to the screen.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnRendered(object sender, RenderedEventArgs e)
+        {
+            if (Game1.activeClickableMenu is TitleMenu && this.ShouldDrawUpdateButton())
+            {
+                this.button.Draw(e.SpriteBatch);
+            }
+        }
+
+
+        /// <summary>If the update button should be drawn</summary>
+        /// <returns>If the update button should be drawn</returns>
+        private bool ShouldDrawUpdateButton()
+        {
+            if (!(Game1.activeClickableMenu is TitleMenu titleMenu))
+            {
+                return false;
+            }
+
+            bool showButton = TitleMenu.subMenu == null && !this.GetPrivateBool(titleMenu, "isTransitioningButtons") &&
+                (this.GetPrivateBool(titleMenu, "titleInPosition") && !this.GetPrivateBool(titleMenu, "transitioningCharacterCreationMenu"));
+
+            return showButton;
+        }
+
+        /// <summary>Gets a private boolean using reflection.</summary>
+        /// <param name="obj">The object to get the boolean from.</param>
+        /// <param name="name">The name of the boolean.</param>
+        /// <returns>The boolean value.</returns>
+        private bool GetPrivateBool(object obj, string name)
+        {
+            return this.Helper.Reflection.GetField<bool>(obj, name).GetValue();
+        }
+
+
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // Check for left mouse button
+            if (e.Button != SButton.MouseLeft)
+            {
+                return;
+            }
+
+            if (this.button.PointContainsButton(e.Cursor.ScreenPixels) && Game1.activeClickableMenu is TitleMenu && TitleMenu.subMenu == null)
+            {
+                // TitleMenu.subMenu = this.menu;
+                // this.menu.Activated();
+                try
+                {
+                    var ps = new ProcessStartInfo("https://nexusmods.com/stardewvalley")
+                    {
+                        UseShellExecute = true,
+                        Verb = "open"
+                    };
+                    Process.Start(ps);
+                    Game1.playSound("bigSelect");
+
+                }
+                catch
+                {
+                    Game1.playSound("toyPiano");
+                }
+            }
+        }
+    }
+}
